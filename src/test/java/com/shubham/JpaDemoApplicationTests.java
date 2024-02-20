a134926dc0c0d6b3ed2839e6197ea9260a8c1119
Shubham Sanjay Chougule <52528575+ShubhamChougule@users.noreply.github.com>
2024-02-20 11:03:55 +0530

    Update JpaDemoApplicationTests.java
    test statement added

--- src/test/java/com/shubham/JpaDemoApplicationTests.java
+++ src/test/java/com/shubham/JpaDemoApplicationTests.java
@@ -2,12 +2,14 @@ package com.shubham;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import java.util.*;
 
 @SpringBootTest
 class JpaDemoApplicationTests {
 
 	@Test
 	void contextLoads() {
+		System.out.println("Testing is Done");
 	}
 
 }
